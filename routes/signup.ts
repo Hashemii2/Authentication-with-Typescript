3efcaea213f5e1e7f59fa0b1c9a42c0f8472cfa4
mahoor <mmahoor59@yahoo.com>
2021-07-03 02:09:11 +0430

    adding validation to signup route

--- routes/signup.ts
+++ routes/signup.ts
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("hi");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("ایمیل موجود نیست."),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("پسورد باید بین 4 تا ۲۰ کاراکتر باشد."),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
